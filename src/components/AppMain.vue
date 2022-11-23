fc49deeec2a7bedf4e8678ea1aa08df819dc7394
matteoga95 <gallo.work95@gmail.com>
2022-11-23 01:56:32 +0100

    sezione world class

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,16 +1,46 @@
 <script>
+import titleDiv from '../components/partial/TitleDivisorio.vue';
 
 export default {
-
+    components: {
+        titleDiv
+    }
 }
 </script>
 
 <template>
     <main>
-        ciao
+        <!-- //sezione semplice di testo -->
+        <section class="simple-text  ">
+            <div class="contenitore ">
+                <titleDiv title="WORLD CLASS Fitness" />
+                <p>At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we're equipped
+                    to
+                    help you reach your goals of a healty and fit lifesyle.</p>
+            </div>
+
+            <span class="triangle-down">&#9207</span>
+
+        </section>
+
     </main>
 </template>
 
 <style lang="scss" scoped>
 @import "../assets/scss/variables.scss";
+
+
+.simple-text {
+    text-align: center;
+    background-color: $greyMain;
+    position: relative;
+}
+
+.triangle-down {
+    color: $greyMain ;
+}
+
+p {
+    color: $greyFont;
+}
 </style>
---
+++ src/components/partial/TitleDivisorio.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    name: "titleDiv",
+    props: {
+        title: String
+    }
+}
+</script>
+
+<template>
+    <h2 class="py-4">{{ this.title }}</h2>
+    <div class="d-flex justify-content-center">
+        <img class="pb-4" src="../../assets/img/divider-xx-red.png" alt="">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables.scss";
+
+h2 {
+    font-weight: 600;
+    font-size: $titleSectionFSize;
+    color: white;
+}
+</style>
